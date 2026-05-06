dcf51f3e99306803fc0467f70828b15383e3ebd9
BharathManikanta <76932040+BharathManikanta@users.noreply.github.com>
2026-05-06 15:01:42 +0530

    Create generate_email.py

---
+++ scripts/generate_email.py
@@ -0,0 +1,25 @@
+from jinja2 import Environment, FileSystemLoader
+import os
+
+
+def generate_email_content(args):
+
+    template_dir = os.path.dirname(__file__)
+
+    env = Environment(
+        loader=FileSystemLoader(template_dir)
+    )
+
+    template = env.get_template(
+        'email-template.jinja'
+    )
+
+    data = {
+        'name': args.name,
+        'status': args.status,
+        'service_name': args.service_name,
+        'build_number': args.build_number,
+        'build_time': args.build_time
+    }
+
+    return template.render(data)
